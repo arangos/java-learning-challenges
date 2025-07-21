66f85d7b35b49f856c4732e2dfba2827b41cb93c
andres.arangom <andres.arango@aptitude.com>
2025-07-21 16:00:24 -0500

    Adding new challenge make string fancy

---
+++ src/main/java/com.codebulls/challenges/DeleteCharacterToMakeFancyString.java
@@ -0,0 +1,35 @@
+package com.codebulls.challenges;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeleteCharacterToMakeFancyString {
+
+    public static void main(String[] args){
+        String input = "leeetcode";
+        System.out.println(makeFancyString(input));
+    }
+
+    public static String makeFancyString(String text){
+        //i need to check if a character is repeated more than 2 times to removed
+        int length = text.length();
+        int count = 0;
+        char prev = '\0';
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int end = 0; end < length; end++) {
+            char character = text.charAt(end);
+            if(character == prev){
+                count++;
+            } else{
+                prev = character;
+                count = 1;
+            }
+
+            if(count < 3) {
+                stringBuilder.append(character);
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
