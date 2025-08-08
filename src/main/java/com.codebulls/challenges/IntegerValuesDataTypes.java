348321f82f735c8ab087139f405305df7e42a729
andres.arangom <andres.arango@aptitude.com>
2025-08-08 13:54:20 -0500

    Refactoring integer values for java constants

--- src/main/java/com.codebulls/challenges/IntegerValuesDataTypes.java
+++ src/main/java/com.codebulls/challenges/IntegerValuesDataTypes.java
@@ -22,13 +22,13 @@ public class IntegerValuesDataTypes {
                 long x = sc.nextLong();
                 System.out.println(x+" can be fitted in:");
                 // Note: The range for byte is from -128 to 127
-                if(x >= -128 && x <= 127) System.out.println("* byte");
+                if(x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) System.out.println("* byte");
                 // Note: The range for short is from -32768 to 32767
-                if(x>= -32768 &&  x <= 32767) System.out.println("* short");
+                if(x>= Short.MIN_VALUE &&  x <= Short.MAX_VALUE) System.out.println("* short");
                 // Note: The range for int is from -2147483648 to 2147483647
-                if(x>= -2147483648 &&  x <= 2147483647) System.out.println("* int");
+                if(x>= -Integer.MIN_VALUE &&  x <= Integer.MAX_VALUE) System.out.println("* int");
                 // Note: The range for long is from -9223372036854775808 to 9223372036854775807
-                if(x>= -9223372036854775808L &&  x <= 9223372036854775807L) System.out.println("* long");
+                if(x>= Long.MIN_VALUE &&  x <= Long.MAX_VALUE) System.out.println("* long");
             }catch(Exception e){
                 System.out.println(sc.next()+" can't be fitted anywhere.");
             }
